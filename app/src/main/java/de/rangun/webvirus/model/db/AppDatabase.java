1e7229d2efeeefd963c182ec01245a74bce84ded
Heiko Schaefer <heiko@rangun.de>
2019-10-11 16:28:13 +0200

    preparations for movie markers room database

---
+++ app/src/main/java/de/rangun/webvirus/model/db/AppDatabase.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 by Heiko Schäfer <heiko@rangun.de>
+ *
+ *  This file is part of android-webvirus.
+ *
+ *  android-webvirus is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  android-webvirus is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with android-webvirus.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Last modified 11.10.19 16:19 by heiko
+ */
+
+package de.rangun.webvirus.model.db;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Movie.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract MovieDao userDao();
+}
